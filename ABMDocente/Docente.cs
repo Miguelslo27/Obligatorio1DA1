2b94427938e1a4aac49bc4c531847b59c118b5aa
Miguel Angel Sosa Menchaca <miguel.sosa@globant.com>
2017-10-10 10:05:36 -0300

    First Commit

---
+++ ABMDocente/Docente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMDocente
+{
+    public class Docente
+    {
+        public void altaDatosDocente()
+        {
+            Console.WriteLine("Alta Docente");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ABMDocente;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testAltaDatosDocente()
+        {
+            var docente = new Docente();
+        }
+    }
+}
